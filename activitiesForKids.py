5c03fa357f69c17499fdbd3dea206d40f4469f83
James Chapman <chapmanjamesg@gmail.com>
2020-01-16 11:13:09 -0600

    first Commit

---
+++ activitiesForKids.py
@@ -0,0 +1,16 @@
+running_kids = ["Pam", "Sam", "Andrea", "Will"]
+swinging_kids = ["Marybeth", "Jenna", "Kevin", "Courtney"]
+sliding_kids = ["Mike", "Jack", "Jennifer", "Earl"]
+hiding_kids = ["Henry", "Heather", "Hayley", "Hugh"]
+
+for kids in running_kids:
+    print(f"{kids} ran like a fool!")
+
+for kids in swinging_kids:
+    print(f"{kids} swang like a maniac!")
+
+for kids in sliding_kids:
+    print(f"{kids} went down the slide with no regard!")
+
+for kids in hiding_kids:
+    print(f"No one could find {kids}, who is the seeker?")
